d01f0c0a822aded854d8ff32f401e563b6e61fd0
Robpol86 <robpol86@gmail.com>
2015-08-22 16:48:17 -0700

    Tweaked test.

--- tests/test_AutoLoader_query.py
+++ tests/test_AutoLoader_query.py
@@ -18,8 +18,8 @@ def test_bad_host(caplog, host):
     assert log.endswith('not known>')
 
 
-@pytest.mark.parametrize('code', [404, 401, 500])
-def test_bad_host_creds(monkeypatch, caplog, code):
+@pytest.mark.parametrize('code', [404, 500])
+def test_http_errors(monkeypatch, caplog, code):
     def urlopen(handler):
         raise urllib2.HTTPError(handler.get_full_url(), code, '', None, None)
     monkeypatch.setattr('urllib2.urlopen', urlopen)
@@ -31,8 +31,6 @@ def test_bad_host_creds(monkeypatch, caplog, code):
     log = caplog.records()[-1].message
     if code == 404:
         expected = '404 Not Found on: http://124t.local/'
-    elif code == 401:
-        expected = '401 Unauthorized (bad credentials?) on: http://124t.local/'
     else:
         expected = 'http://124t.local/ returned HTTP {} instead of 200.'.format(code)
     assert log == expected
